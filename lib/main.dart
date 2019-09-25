1c5b5f667ffb092b34ae1320f1e97be432ea6c95
AjiSetya <setyaaji07@gmail.com>
2019-09-25 13:49:49 +0700

    tampilan kartu nama

--- lib/main.dart
+++ lib/main.dart
@@ -8,35 +8,76 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("kartu Nama"),
-          backgroundColor: Colors.blueGrey,
-        ),
-        body: Row(
-          // mengubah posisi anak secara horizontal
-          crossAxisAlignment: CrossAxisAlignment.center,
-          // mengubah posisi anak secara vertikal
-          mainAxisAlignment: MainAxisAlignment.center,
-          //mengurutkan anak dari ats ke bawah/sebaliknya
-          verticalDirection: VerticalDirection.down,
-          // mengatur tinggi si column (maksimal/minimal)
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Expanded(
-                child: Image(
-              image: AssetImage("image/img_error.png"),
-            )),
-            Text("Gambar Rusak"),
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.blue,
-              child: Text("Kontainer 1"),
-            )
-          ],
-        ),
-      ),
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: Text("kartu Nama"),
+            backgroundColor: Colors.blueGrey,
+          ),
+          body: tampilanUtama),
       debugShowCheckedModeBanner: false,
     );
   }
+
+  final tampilanUtama = new Center(
+      child: new Column(
+    // mengubah posisi anak secara horizontal
+    crossAxisAlignment: CrossAxisAlignment.center,
+    // mengubah posisi anak secara vertikal
+    mainAxisAlignment: MainAxisAlignment.center,
+    //mengurutkan anak dari ats ke bawah/sebaliknya
+    verticalDirection: VerticalDirection.down,
+    // mengatur tinggi si column (maksimal/minimal)
+    mainAxisSize: MainAxisSize.max,
+    children: <Widget>[
+      CircleAvatar(
+        backgroundImage: NetworkImage(
+            "https://i.pinimg.com/originals/64/8c/d7/648cd749759267f65925c344b43e1613.jpg"),
+        radius: 50.0,
+      ),
+      Text(
+        "M Ilham Setya Aji",
+        style: TextStyle(
+            fontSize: 30.0,
+            color: Colors.blueGrey,
+            fontWeight: FontWeight.bold),
+      ),
+      Text(
+        "NISN / NIS",
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+      ),
+      Text("7887778 / 0809796"),
+      RaisedButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          child: Text("setyaaji07@gmail.com"),
+          color: Colors.blueGrey,
+          textColor: Colors.white,
+          onPressed: () {}),
+      RaisedButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          child: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: <Color>[
+                  Color(0xFF0D47A1),
+                  Color(0xFF1976D2),
+                  Color(0xFF42A5F5),
+                ]),
+                borderRadius: BorderRadius.all(Radius.circular(80.0))),
+            child: Text("089630074842"),
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+          ),
+          textColor: Colors.white,
+          padding: const EdgeInsets.all(0.0),
+          onPressed: () {}),
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: Text("Alamat"),
+      ),
+      Text(
+        "Jl. Raya Dayeuh, Sukanegara, Kec. Jonggol, Bogor, Jawa Barat 16830",
+        textAlign: TextAlign.center,
+      ),
+    ],
+  ));
 }
